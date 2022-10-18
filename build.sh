11cd020a7922771d41a21b3fdb19f50401ec0691
Meinte Boersma <meinte.boersma@gmail.com>
2022-10-18 14:31:46 +0200

    make deployment on Vercel work

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-rm dist/*
-npx parcel build src/index.html
-sed -e 's/="\/index/="file:\.\/index/g' dist/index.html > dist/index-local.html
-sed -i -e 's/type="module"//g' dist/index-local.html
-open dist/index-local.html
-rm dist/index-local.html-e
+npx parcel build src/index.html --dist-dir build
+sed -e 's/="\/index/="file:\.\/index/g' build/index.html > build/index-local.html
+sed -i -e 's/type="module"//g' build/index-local.html
+#rm build/index-local.html-e
+#open build/index-local.html
